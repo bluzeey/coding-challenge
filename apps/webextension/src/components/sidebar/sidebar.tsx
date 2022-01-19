56ee10262e1e3c783ebb6f68c36494a01e8c3085
Sahil Maheshwari <sahilm1711@gmail.com>
2022-01-19 20:25:38 +0530

    commiting changes

--- apps/webextension/src/components/sidebar/sidebar.tsx
+++ apps/webextension/src/components/sidebar/sidebar.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable-next-line */
-import ClearIcon from '@mui/icons-material/Clear';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import InfoSharpIcon from '@mui/icons-material/InfoSharp';
-import DoneIcon from '@mui/icons-material/Done';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes,faInfoCircle,faSortDown,faCheck} from '@fortawesome/free-solid-svg-icons'
 export interface SidebarProps {}
 
 export function Sidebar(props: SidebarProps) {
@@ -17,23 +15,23 @@ export function Sidebar(props: SidebarProps) {
   return (
     <div className="font-inter bg-slate-50 flex flex-col p-5 max-w-sm rounded shadow-md">
       <div className="flex justify-end">
-      <ClearIcon fontSize="large" className="text-white bg-red-600 rounded shadow-md cursor-pointer"/>
+        <FontAwesomeIcon className="bg-red-500 text-white px-1  rounded" icon={faTimes} size="2x" />
       </div>
       <div className="mb-2 mt-2">
-         <h1 className="font-bold">Duration</h1>
-         <p>30 min<ArrowDropDownIcon/></p>
+         <h1 className="font-bold">Duration <FontAwesomeIcon className="text-blue-400 rounded" icon={faInfoCircle}/></h1>
+         <p>30 min</p>
       </div>
       <div className="mb-2 mt-2">
-        <h1 className="font-bold">Event type <InfoSharpIcon className="text-blue-400 shadow-md rounded-full cursor-pointer"/></h1>
+        <h1 className="font-bold">Event type</h1>
         <div className="flex justify-between mb-2 mt-2">
           <p className="font-bold">Share Slots</p>
-          <ArrowDropDownIcon/>
+          <FontAwesomeIcon className="" icon={faSortDown}/>
         </div>
       </div>
       <div className="flex flex-col mt-2 mb-2">
         <div className="flex justify-between">
           <h1 className="font-bold">Save placeholders</h1>
-          <DoneIcon className="text-white bg-blue-400 rounded font-bold shadow-md"/>
+          <FontAwesomeIcon className="bg-blue-400 text-white p-1 rounded-md" icon={faCheck} size="lg"/>
         </div>
         <textarea rows={2} id="title" className="text-xs rounded-lg mt-2 p-2 border-2 border-gray-300 mb-2 placeholder:text-xs" placeholder="Type Placeholder Name Here"></textarea>
         <textarea rows={7} id="events"  className="text-xs rounded-lg mt-2 p-2 border-2 border-gray-300 mb-2 placeholder:text-xs placeholder:m-2" placeholder="Start selecting availiabilites you would like to share on the calendar"></textarea>
